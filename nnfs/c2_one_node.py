c21016ca559c5e78963234ed975b77912ec80a73
JennEYoon <jenneyoon@gmail.com>
2022-08-11 20:53:41 -0400

    Add nnfs chp2 practice files

---
+++ nnfs/c2_one_node.py
@@ -0,0 +1,11 @@
+# nn one node  
+# nnfs  
+# 8/11/2022  
+
+inputs = [1, 2, 3] 
+weights = [0.2, 0.8, -0.5 ]
+bias = 2
+print(inputs, weights, bias)
+
+output = (inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias)
+print(output)
