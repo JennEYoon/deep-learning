996977296e3b7199453a5b8ec7108ebb2e926c7e
JennEYoon <jenneyoon@gmail.com>
2023-02-05 18:14:30 -0500

    Update pandas code

--- practice/pandas_ex1.py
+++ practice/pandas_ex1.py
@@ -11,10 +11,18 @@ print(ind, type(ind))
 
 print(ind[2])   # ith item
 print(ind[-1])  # last item
-print(ind[::3])  # repeat every 3rd starting at 0th.
+print(ind[::3])  # repeat every 3rd starting at 0th.  
+
+### reversed repeat
 print(ind[2::-1])  # reversed repeat, count from -2 not 0. [8, 7, 6, ...] I thinkt this is correct.  
-# or it could be reversed from 2 to 10, [10, 9, 8, .. 3, 2]
-print(ind{::-1])  # this reverses the whole thing starting from last item.   
+          # or it could be reversed from 2 to 10, [10, 9, 8, .. 3, 2]
+print(ind[::-1])  # this reverses the whole thing starting from last item.   
           # so likely count backwards, starting from last as 0th.  
+# >>> Wrong! [2, 1, 0].  Start from ith, the reverse by step one repeatedly.  
+print(ind[8::-2])   # expect [8, 6, 4, 2, 0]  Start from ith, reverse by step -n, repeat. 
           
-          
+### [i::+/-n]  Start from ith from left, then repeat (2nd :) every nth, or -nth.  
+print(ind[0::-2])  # only gives "[0]"  
+# so [::-1] must be a special case. if 0th is not specified, start from last.  
+print(ind[::-2])  # in reverse, not specified start is same as start from last  
+print(ind[-1::-2])  # count from right assumed in reverse flow, if omitted.  
